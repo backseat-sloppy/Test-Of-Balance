5c2114a6ed2391174d64cf56b726fa60ade4eac7
backseat-sloppy <147695981+backseat-sloppy@users.noreply.github.com>
2024-11-17 12:07:16 +0100

    s

--- Assets/Scripts/Classes for Spell Casting/Spells/FireBall01.cs
+++ Assets/Scripts/Classes for Spell Casting/Spells/FireBall01.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class FireBall01 : Spell
 {
-
+    private Vector3 castPoint;
+    private Quaternion castRotation;
+    public GameObject Fireball;
     public FireBall01()
     {
         Name = "Fire Ball";
@@ -12,11 +14,12 @@ public class FireBall01 : Spell
         Cooldown = 5f;
         ManaCost = 10f;
         CastTime = 0.5f;
+        
     }
 
     public override void Cast()
     {
-       GameObject fireBall = Instantiate(fireballPrefab, castPoint.position, castPoint.rotation) as GameObject;
+       GameObject fireBall = Instantiate(Fireball, castPoint.position, castRotation.rotation) as GameObject;
 
         Debug.Log("Castinng " + name);
     }
